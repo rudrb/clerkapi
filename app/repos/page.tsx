70d21637d136a25b520cd036ac7e79ef66415f79
rudrb <aromi154@naver.com>
2024-10-29 11:23:17 +0900

    Add files via upload

--- app/repos/page.tsx
+++ app/repos/page.tsx
@@ -3,7 +3,7 @@ import { FaCodeBranch, FaEye, FaStar } from 'react-icons/fa'
 const username = 'bradtraversy'
 
 export default async function ReposPage() {
-  const response = await fetch(`https://api.github.com/users/${username}/repos`)
+  const response = await fetch('https://github.com/rudrb/clerkapi')
   const repos = await response.json()
 
   //console.log(repos)
@@ -14,23 +14,26 @@ export default async function ReposPage() {
         Github repositories of {username}
       </h2>
       <ul>
-        {repos.map((repo: any) => (
-          <li key={repo.id} className="bg-gray-100 m-4 p-4 rounded-md">
-            <h3 className="text-xl font-bold">{repo.name}</h3>
-            <p>{repo.description}</p>
-            <div className="flex items-center justify-between">
-              <span className="flex items-center gap-1">
-                <FaStar /> {repo.stargazers_count}
-              </span>
-              <span className="flex items-center gap-1">
-                <FaCodeBranch /> {repo.forks_count}
-              </span>
-              <span className="flex items-center gap-1">
-                <FaEye /> {repo.watchers_count}
-              </span>
-            </div>
-          </li>
-        ))}
+        {
+          /* eslint-disable @typescript-eslint/no-explicit-any */
+          repos.map((repo: any) => (
+            <li key={repo.id} className="bg-gray-100 m-4 p-4 rounded-md">
+              <h3 className="text-xl font-bold">{repo.name}</h3>
+              <p>{repo.description}</p>
+              <div className="flex items-center justify-between">
+                <span className="flex items-center gap-1">
+                  <FaStar /> {repo.stargazers_count}
+                </span>
+                <span className="flex items-center gap-1">
+                  <FaCodeBranch /> {repo.forks_count}
+                </span>
+                <span className="flex items-center gap-1">
+                  <FaEye /> {repo.watchers_count}
+                </span>
+              </div>
+            </li>
+          ))
+        }
       </ul>
     </div>
   )
